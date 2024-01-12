e9d9ca68b9e2b0d60ac25321438d107a070effcd
Yunus Emre ASLAN <40874281+Dubium0@users.noreply.github.com>
2024-01-12 18:39:52 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Doctor> doctors  =  UserController.getFilteredDoctors("Cardiologist",1 ,4,9,12);
-        for(int i = 0 ; i< doctors.size() ;i++){
-            System.out.println(doctors.get(i).toString());
-        }
+      
 
     }
 }
